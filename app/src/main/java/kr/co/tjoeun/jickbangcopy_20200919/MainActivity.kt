849bcc2514bc577b1f16ab638a8583536442f74a
JiHwan Shin <jumprope123@naver.com>
2020-09-19 16:21:43 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/jickbangcopy_20200919/MainActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.jickbangcopy_20200919
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+//        버튼 이벤트 / 리스트뷰 이벤트 등 이벤트 처리만 모아두는 코드
+    }
+
+    override fun setValues() {
+//        .text 값 대입 등 화면 문구/ 데이터 반영하는 코드만
+    }
+}
